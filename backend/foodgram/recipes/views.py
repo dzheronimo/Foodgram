2278e28161d0fe96af18cdac354bc79358c7b0ac
Dzheronimo <dzheronimo@me.com>
2023-06-09 15:55:29 +1000

    добавлен @action метод favorite -- добавление/удаление в избранное

--- backend/foodgram/recipes/views.py
+++ backend/foodgram/recipes/views.py
@@ -37,7 +37,35 @@ class RecipeViewSet(ModelViewSet):
     pagination_class = StandartResultsSetPagination
     permission_classes = [permissions.AllowAny, ]
 
+    @action(detail=True,
+            methods=['POST', 'DELETE'],
+            permission_classes=[permissions.AllowAny, ])
+    def favorite(self, request, pk=None):
+        user = request.user
+        recipe = get_object_or_404(Recipe, id=pk)
+        favorited = FavoriteRecipes.objects.filter(user=user, recipe=recipe)
+
+        if request.method == 'POST':
+            if favorited.exists():
+                return Response(
+                    {"is_favorited": "Рецепт уже находится в избранном"},
+                    status=status.HTTP_400_BAD_REQUEST
+                )
+            FavoriteRecipes.objects.create(user=user, recipe=recipe)
+            serializer = ShortRecipeSerializer(recipe)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+        if request.method == 'DELETE':
+            if favorited.exists():
+                favorited.delete()
+                return Response(
+                    {"is_favorited": "Рецепт удален из списка избранных."},
+                    status=status.HTTP_204_NO_CONTENT)
+            else:
+                return Response(
+                    {"is_favorited": "Рецепт не найден в списке избранных."},
+                    status=status.HTTP_400_BAD_REQUEST
+                )
+
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
-
-
