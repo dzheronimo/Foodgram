f9671d9e6c52f529041e460005f301709c5b7475
Dzheronimo <dzheronimo@me.com>
2023-06-09 15:53:25 +1000

    regex syntax fix

--- backend/foodgram/api/urls.py
+++ backend/foodgram/api/urls.py
@@ -6,14 +6,12 @@ from recipes.views import ListTagViewSet, RecipeViewSet, IngredientsViewSet
 router = DefaultRouter()
 
 router.register('tags', ListTagViewSet)
-router.register(r'tags/?P(<id>\d+)', ListTagViewSet)
 router.register('recipes', RecipeViewSet, basename='recipes')
 router.register('ingredients', IngredientsViewSet)
-router.register('ingredients/?P(<id>\d+)', IngredientsViewSet)
+router.register('ingredients/(?P<id>\d+)', IngredientsViewSet)
 
 
 urlpatterns = [
     path('', include('users.urls'), name='users'),
     path('', include(router.urls)),
-    # path('tags/', ListTag.as_view(), name='tag')
 ]
