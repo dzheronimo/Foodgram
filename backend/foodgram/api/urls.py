d3df426f2740a13b79ae1ed4cedeb9a83dc3bf77
Dzheronimo <dzheronimo@me.com>
2023-06-11 20:42:39 +1000

    Скорректировано расположение path ля роутеров и djoser.urls

--- backend/foodgram/api/urls.py
+++ backend/foodgram/api/urls.py
@@ -9,11 +9,10 @@ router.register('tags', ListTagViewSet)
 router.register('recipes', RecipeViewSet, basename='recipes')
 router.register('ingredients', IngredientsViewSet)
 router.register('ingredients/(?P<id>\d+)', IngredientsViewSet)
-router.register('users/subscriptions', SubscriptionViewSet)
 router.register('users', SubscriptionViewSet)
 
 
 urlpatterns = [
-    path('', include('users.urls'), name='users'),
     path('', include(router.urls)),
+    path('', include('users.urls'), name='users'),
 ]
