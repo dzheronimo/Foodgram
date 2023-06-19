6755acd278e04e83b536bda1fd0197f284c78c7e
Dzheronimo <dzheronimo@me.com>
2023-06-20 01:56:49 +1000

    PEP8

--- backend/foodgram/recipes/views.py
+++ backend/foodgram/recipes/views.py
@@ -43,7 +43,7 @@ class IngredientsViewSet(ReadOnlyModelViewSet):
     serializer_class = IngredientSerializer
     permission_classes = [permissions.AllowAny, ]
     filter_backends = [IngredientSearchFilter, ]
-    search_fields = ['$\S', ]
+    search_fields = ['$\S+', ]
 
 
 class RecipeViewSet(ModelViewSet):
