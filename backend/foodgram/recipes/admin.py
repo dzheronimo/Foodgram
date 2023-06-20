32279c8b1d076781e2b77d70e7efe817718c4dc2
Dzheronimo <dzheronimo@me.com>
2023-06-20 23:11:17 +1000

    Удалены несвязанные inline модели.

--- backend/foodgram/recipes/admin.py
+++ backend/foodgram/recipes/admin.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 from .models import (
     Tag, Recipe, Ingredient,
     IngredientAmountRecipe, FavoriteRecipes,
-    ShoppingCart, Subscription
+    ShoppingCart
 )
 
 
