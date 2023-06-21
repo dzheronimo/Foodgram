c3b710f9a190053e6b4c5fe2874d3449134805fd
Dzheronimo <dzheronimo@me.com>
2023-06-21 22:08:31 +1000

    search_fields у IngredientsViewSet теперь используется полный поиск

--- backend/foodgram/recipes/views.py
+++ backend/foodgram/recipes/views.py
@@ -43,7 +43,7 @@ class IngredientsViewSet(ReadOnlyModelViewSet):
     serializer_class = IngredientSerializer
     permission_classes = [permissions.AllowAny, ]
     filter_backends = [IngredientSearchFilter, ]
-    search_fields = ['$name', ]
+    search_fields = ['name', ]
 
 
 class RecipeViewSet(ModelViewSet):
